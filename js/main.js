37cdcbdf528d6012881273e0a3ae9607dd3c078f
Kevin Soto <dev@kevinsoto.ar>
2023-10-12 10:01:51 -0300

    Switch cookies for localStorage

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ try
     let themePicker = document.querySelector('#theme-select');
     themePicker.addEventListener('change', () => {
         document.querySelector('#site-theme').setAttribute('href', 'css/themes/'+themePicker.value);
-        document.cookie = "websiteTheme="+themePicker.value;
+        localStorage.setItem("websiteTheme", themePicker.value);
     });
 
     // Set theme depending on if the cookie "websiteTheme" is set
-    let theme = getCookie("websiteTheme");
+    let theme = localStorage.getItem("websiteTheme");
     if(theme !== null) {
         themePicker.value = theme;
         document.querySelector('#site-theme').setAttribute('href', 'css/themes/'+theme);
@@ -63,21 +63,4 @@ function disableButton(button) {
 // Get a random integer
 function randomInt(rangeInt, startInt = 0) {
     return startInt + Math.floor(Math.random() * rangeInt);
-}
-
-// Copy-pasted from w3schools.com: https://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-    }
-    }
-    return null;
 }
